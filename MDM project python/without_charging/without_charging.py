03add3de193a9e95134fab41de3b476b2fe24d4a
VivekIOT <vijayanv31@gmail.com>
2017-10-28 21:53:16 +0530

    Update without_charging.py

--- MDM project python/without_charging/without_charging.py
+++ MDM project python/without_charging/without_charging.py
@@ -156,10 +156,11 @@ def write_to_file(data):
 def searching(x,y,r,times):
 	global visited
 	global n
+	
 	if(N1.check!=1):
 		z=math.sqrt((pow(N1.xpos-x,2))+pow(N1.ypos-y,2))
 		if(z < (N1.radius+r)):
-			w=("\t\tNode1 Found at [xpos:%s][ypos:%s]  \n\t\tNode1 -> Charge:%d"%(N1.xpos,N1.ypos,N1.charge))
+			w=("\t\tNode1 Found at [xpos:%s][ypos:%s]  \n\t\tNode1 -> Charge:%d %%"%(N1.xpos,N1.ypos,N1.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N1.check=1
@@ -169,12 +170,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N1.charger(N1.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 
 	if(N2.check!=1):
 		z=math.sqrt((pow(N2.xpos-x,2))+pow(N2.ypos-y,2))
 		if(z < (N2.radius+r)):
-			w=("\t\tNode2 Found at [xpos:%s][ypos:%s]  \n\t\tNode2 -> Charge:%d"%(N2.xpos,N2.ypos,N2.charge))
+			w=("\t\tNode2 Found at [xpos:%s][ypos:%s]  \n\t\tNode2 -> Charge:%d %%"%(N2.xpos,N2.ypos,N2.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N2.check=1
@@ -184,12 +185,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N2.charger(N2.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N3.check!=1):
 		z=math.sqrt((pow(N3.xpos-x,2))+pow(N3.ypos-y,2))
 		if(z < (N3.radius+r)):
-			w=("\t\tNode3 Found at [xpos:%s][ypos:%s]  \n\t\tNode3 -> Charge:%d"%(N3.xpos,N3.ypos,N3.charge))
+			w=("\t\tNode3 Found at [xpos:%s][ypos:%s]  \n\t\tNode3 -> Charge:%d %%"%(N3.xpos,N3.ypos,N3.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N3.check=1
@@ -199,11 +200,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N3.charger(N3.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
+		
 	if(N4.check!=1):
 		z=math.sqrt((pow(N4.xpos-x,2))+pow(N4.ypos-y,2))
 		if(z < (N4.radius+r)):
-			w=("\t\tNode4 Found at [xpos:%s][ypos:%s]  \n\t\tNode4 -> Charge:%d"%(N4.xpos,N4.ypos,N4.charge))
+			w=("\t\tNode4 Found at [xpos:%s][ypos:%s]  \n\t\tNode4 -> Charge:%d %%"%(N4.xpos,N4.ypos,N4.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N4.check=1
@@ -213,12 +215,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N4.charger(N4.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N5.check!=1):
 		z=math.sqrt((pow(N5.xpos-x,2))+pow(N5.ypos-y,2))
 		if(z < (N5.radius+r)):
-			w=("\t\tNode5 Found at [xpos:%s][ypos:%s]  \n\t\tNode5 -> Charge:%d"%(N5.xpos,N5.ypos,N5.charge))
+			w=("\t\tNode5 Found at [xpos:%s][ypos:%s]  \n\t\tNode5 -> Charge:%d %%"%(N5.xpos,N5.ypos,N5.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N5.check=1
@@ -228,12 +230,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N5.charger(N5.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N6.check!=1):
 		z=math.sqrt((pow(N6.xpos-x,2))+pow(N6.ypos-y,2))
 		if(z < (N6.radius+r)):
-			w=("\t\tNode6 Found at [xpos:%s][ypos:%s]  \n\t\tNode6 -> Charge:%d"%(N6.xpos,N6.ypos,N6.charge))
+			w=("\t\tNode6 Found at [xpos:%s][ypos:%s]  \n\t\tNode6 -> Charge:%d %%"%(N6.xpos,N6.ypos,N6.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N6.check=1
@@ -243,12 +245,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N6.charger(N6.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N7.check!=1):
 		z=math.sqrt((pow(N7.xpos-x,2))+pow(N7.ypos-y,2))
 		if(z < (N7.radius+r)):
-			w=("\t\tNode7 Found at [xpos:%s][ypos:%s]  \n\t\tNode7 -> Charge:%d"%(N7.xpos,N7.ypos,N7.charge))
+			w=("\t\tNode7 Found at [xpos:%s][ypos:%s]  \n\t\tNode7 -> Charge:%d %%"%(N7.xpos,N7.ypos,N7.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N7.check=1
@@ -258,12 +260,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N7.charger(N7.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N8.check!=1):
 		z=math.sqrt((pow(N8.xpos-x,2))+pow(N8.ypos-y,2))
 		if(z < (N8.radius+r)):
-			w=("\t\tNode8 Found at [xpos:%s][ypos:%s]  \n\t\tNode8 -> Charge:%d"%(N8.xpos,N8.ypos,N8.charge))
+			w=("\t\tNode8 Found at [xpos:%s][ypos:%s]  \n\t\tNode8 -> Charge:%d %%"%(N8.xpos,N8.ypos,N8.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N8.check=1
@@ -273,12 +275,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N8.charger(N8.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N9.check!=1):
 		z=math.sqrt((pow(N9.xpos-x,2))+pow(N9.ypos-y,2))
 		if(z < (N9.radius+r)):
-			w=("\t\tNode9 Found at [xpos:%s][ypos:%s]  \n\t\tNode9 -> Charge:%d"%(N9.xpos,N9.ypos,N9.charge))
+			w=("\t\tNode9 Found at [xpos:%s][ypos:%s]  \n\t\tNode9 -> Charge:%d %%"%(N9.xpos,N9.ypos,N9.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N9.check=1
@@ -288,11 +290,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N9.charger(N9.charge)
 				mfile.write("\tCharged by Mobile node")
-						
+				times=times+4
+		
 	if(N10.check!=1):
 		z=math.sqrt((pow(N10.xpos-x,2))+pow(N10.ypos-y,2))
 		if(z < (N10.radius+r)):
-			w=("\t\tNode10 Found at [xpos:%s][ypos:%s]  \n\t\tNode10 -> Charge:%d"%(N10.xpos,N10.ypos,N10.charge))
+			w=("\t\tNode10 Found at [xpos:%s][ypos:%s]  \n\t\tNode10 -> Charge:%d %%"%(N10.xpos,N10.ypos,N10.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N10.check=1
@@ -302,12 +305,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N10.charger(N10.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N11.check!=1):
 		z=math.sqrt((pow(N11.xpos-x,2))+pow(N11.ypos-y,2))
 		if(z < (N11.radius+r)):
-			w=("\t\tNode11 Found at [xpos:%s][ypos:%s]  \n\t\tNode11 -> Charge:%d"%(N11.xpos,N11.ypos,N11.charge))
+			w=("\t\tNode11 Found at [xpos:%s][ypos:%s]  \n\t\tNode11 -> Charge:%d %%"%(N11.xpos,N11.ypos,N11.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N11.check=1
@@ -317,13 +320,13 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N11.charger(N11.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 
 	if(N12.check!=1):
 		z=math.sqrt((pow(N12.xpos-x,2))+pow(N12.ypos-y,2))
 		if(z < (N12.radius+r)):
-			w=("\t\tNode12 Found at [xpos:%s][ypos:%s]  \n\t\tNode12 -> Charge:%d"%(N12.xpos,N12.ypos,N12.charge))
+			w=("\t\tNode12 Found at [xpos:%s][ypos:%s]  \n\t\tNode12 -> Charge:%d %%"%(N12.xpos,N12.ypos,N12.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N12.check=1
@@ -333,12 +336,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N12.charger(N12.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N13.check!=1):
 		z=math.sqrt((pow(N13.xpos-x,2))+pow(N13.ypos-y,2))
 		if(z < (N13.radius+r)):
-			w=("\t\tNode13 Found at [xpos:%s][ypos:%s]  \n\t\tNode13 -> Charge:%d"%(N13.xpos,N13.ypos,N13.charge))
+			w=("\t\tNode13 Found at [xpos:%s][ypos:%s]  \n\t\tNode13 -> Charge:%d %%"%(N13.xpos,N13.ypos,N13.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N13.check=1
@@ -348,12 +351,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N13.charger(N13.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N14.check!=1):
 		z=math.sqrt((pow(N14.xpos-x,2))+pow(N14.ypos-y,2))
 		if(z < (N14.radius+r)):
-			w=("\t\tNode14 Found at [xpos:%s][ypos:%s]  \n\t\tNode14 -> Charge:%d"%(N14.xpos,N14.ypos,N14.charge))
+			w=("\t\tNode14 Found at [xpos:%s][ypos:%s]  \n\t\tNode14 -> Charge:%d %%"%(N14.xpos,N14.ypos,N14.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N14.check=1
@@ -363,12 +366,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N14.charger(N14.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N15.check!=1):
 		z=math.sqrt((pow(N15.xpos-x,2))+pow(N15.ypos-y,2))
 		if(z < (N15.radius+r)):
-			w=("\t\tNode15 Found at [xpos:%s][ypos:%s]  \n\t\tNode15 -> Charge:%d"%(N15.xpos,N15.ypos,N15.charge))
+			w=("\t\tNode15 Found at [xpos:%s][ypos:%s]  \n\t\tNode15 -> Charge:%d %%"%(N15.xpos,N15.ypos,N15.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N15.check=1
@@ -378,12 +381,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N15.charger(N15.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N16.check!=1):
 		z=math.sqrt((pow(N16.xpos-x,2))+pow(N16.ypos-y,2))
 		if(z < (N16.radius+r)):
-			w=("\t\tNode16 Found at [xpos:%s][ypos:%s]  \n\t\tNode16 -> Charge:%d"%(N16.xpos,N16.ypos,N16.charge))
+			w=("\t\tNode16 Found at [xpos:%s][ypos:%s]  \n\t\tNode16 -> Charge:%d %%"%(N16.xpos,N16.ypos,N16.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N16.check=1
@@ -393,12 +396,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N16.charger(N16.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N17.check!=1):
 		z=math.sqrt((pow(N17.xpos-x,2))+pow(N17.ypos-y,2))
 		if(z < (N17.radius+r)):
-			w=("\t\tNode17 Found at [xpos:%s][ypos:%s]  \n\t\tNode17 -> Charge:%d"%(N17.xpos,N17.ypos,N17.charge))
+			w=("\t\tNode17 Found at [xpos:%s][ypos:%s]  \n\t\tNode17 -> Charge:%d %%"%(N17.xpos,N17.ypos,N17.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N17.check=1
@@ -408,12 +411,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N17.charger(N17.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N18.check!=1):
 		z=math.sqrt((pow(N18.xpos-x,2))+pow(N18.ypos-y,2))
 		if(z < (N18.radius+r)):
-			w=("\t\tNode18 Found at [xpos:%s][ypos:%s]  \n\t\tNode18 -> Charge:%d"%(N18.xpos,N18.ypos,N18.charge))
+			w=("\t\tNode18 Found at [xpos:%s][ypos:%s]  \n\t\tNode18 -> Charge:%d %%"%(N18.xpos,N18.ypos,N18.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N18.check=1
@@ -423,12 +426,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N18.charger(N18.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N19.check!=1):
 		z=math.sqrt((pow(N19.xpos-x,2))+pow(N19.ypos-y,2))
 		if(z < (N19.radius+r)):
-			w=("\t\tNode19 Found at [xpos:%s][ypos:%s]  \n\t\tNode19 -> Charge:%d"%(N19.xpos,N19.ypos,N19.charge))
+			w=("\t\tNode19 Found at [xpos:%s][ypos:%s]  \n\t\tNode19 -> Charge:%d %%"%(N19.xpos,N19.ypos,N19.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N19.check=1
@@ -438,12 +441,12 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N19.charger(N19.charge)
 				mfile.write("\tCharged by Mobile node")
-				
+				times=times+4
 		
 	if(N20.check!=1):
 		z=math.sqrt((pow(N20.xpos-x,2))+pow(N20.ypos-y,2))
 		if(z < (N20.radius+r)):
-			w=("\t\tNode20 Found at [xpos:%s][ypos:%s]  \n\t\tNode20 -> Charge:%d"%(N20.xpos,N20.ypos,N20.charge))
+			w=("\t\tNode20 Found at [xpos:%s][ypos:%s]  \n\t\tNode20 -> Charge:%d %%"%(N20.xpos,N20.ypos,N20.charge))
 			mfile.write("\n\n"+w)
 			print(w)
 			N20.check=1
@@ -453,7 +456,7 @@ def searching(x,y,r,times):
 				print("\t\tLow Battery...")
 				N20.charger(N20.charge)
 				mfile.write("\tCharged by Mobile node")
-	
+				times=times+4
 	if(N21.check!=1):
 		N21.update_time(times)
 	
